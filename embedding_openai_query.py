54d4f40767dee6e7e3abf51a732e21b3a911f263
@@tifa Rizvi <rizviaatifa235@gmail.com>
2026-06-09 08:20:57 +0530

    Update embedding_openai_query.py

--- embedding_openai_query.py
+++ embedding_openai_query.py
@@ -1,1 +1,11 @@
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large", dimensions=32)
+
+result = embeddings.embed_query("Delhi is the capital of India.")
+
+print(str(result))
